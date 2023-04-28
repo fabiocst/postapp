34048637c2294d81c78f6af919fb2014817ec49b
Fabio_cst <94999469+fabiocst@users.noreply.github.com>
2023-04-28 20:54:35 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 //Conexão com o banco de dados
-const sequelize = new Sequelize('postapp','root','root',{
- host: "localhost",
- port: "3306",
+const sequelize = new Sequelize('epiz_34100722_postapp','epiz_34100722','f5b7cst96',{
+ host: "sql304.epizy.com",
+ port: "80",
  dialect: 'mysql'
 });
 //Vamos exportar as variáveis
